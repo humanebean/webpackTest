08b4db13d9808515c8910aadb7965cbb754b06a3
Danyal Sheikh <60336684+humanebean@users.noreply.github.com>
2024-02-24 16:31:40 -0600

    now the external library (lodash) is imported and webpack is used to transpile the code into a main.js file which is referred to in index.html. (instead of index.js)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import _ from 'lodash';
 function component(){
     const element = document.createElement('div');
 
     //lodash is included via a script and requires this line to work
-    element.innerHtml = _.join(['Hello', 'webpack'], ' ');
+    //element.innerHtml = _.join(['Hello', 'webpack'], ' ');
+
+    //lodash is now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
     return element;
 }
